be2d604953d77cd6bff21e82463b5d6b0bd293b2
rubens junio <rubens.marques@hotmart.com>
2025-04-26 14:50:23 -0300

    feat: added testing end change video

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -2,6 +2,9 @@ import * as Collapsible from '@radix-ui/react-collapsible'
 
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from './Lesson'
+import { useAppSelector } from '../store'
+import { useDispatch } from 'react-redux'
+import { play } from '../store/slices/player'
 
 interface ModuleProps {
   moduleIndex: number
@@ -10,6 +13,18 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const dispatch = useDispatch()
+
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })
+
+  const lessons = useAppSelector((state) => {
+    return state.player.course.modules[moduleIndex].lessons
+  })
+
   return (
     <Collapsible.Root className="group">
       <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4 cursor-pointer">
@@ -27,9 +42,21 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          <Lesson title="Fundamentos do Redux" duration="09:13" />
-          <Lesson title="Fundamentos do Redux" duration="09:13" />
-          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          {lessons.map((lesson, lessonIndex) => {
+            const isCurrent =
+              currentModuleIndex === moduleIndex &&
+              currentLessonIndex === lessonIndex
+
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                duration={lesson.duration}
+                isCurrent={isCurrent}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+              />
+            )
+          })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,25 @@
 import ReactPlayer from 'react-player'
 
+import { useDispatch } from 'react-redux'
+import { next } from '../store/slices/player'
+import { useCurrentLesson } from '../store/hooks/useCurrentLesson'
+
 export function Video() {
+  const { currentLesson } = useCurrentLesson()
+  const dispatch = useDispatch()
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+        onEnded={handlePlayNext}
+        playing
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   )
