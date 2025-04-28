c506557596655bc995a8c16ec97bcaedba854b3e
rubens junio <rubens.marques@hotmart.com>
2025-04-28 14:35:29 -0300

    added asycn thunk

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,17 +3,27 @@ import { MessageCircle } from 'lucide-react'
 import { Header } from '../components/Header'
 import { Video } from '../components/Video'
 import { Module } from '../components/Module'
-import { useAppSelector } from '../store'
+import { useAppDispatch, useAppSelector } from '../store'
+import { loadCourse, useCurrentLesson } from '../store/slices/player'
 import { useEffect } from 'react'
-import { useCurrentLesson } from '../store/hooks/useCurrentLesson'
 
 export function Player() {
-  const modules = useAppSelector((state) => state.player.course.modules)
+  const dispatch = useAppDispatch()
+
+  const modules = useAppSelector((state) => {
+    return state.player.course?.modules
+  })
 
   const { currentLesson } = useCurrentLesson()
 
   useEffect(() => {
-    document.title = `Assistindo: ${currentLesson.title}`
+    dispatch(loadCourse())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (currentLesson) {
+      document.title = `Assistindo: ${currentLesson.title}`
+    }
   }, [currentLesson])
 
   return (
@@ -32,17 +42,18 @@ export function Player() {
           <div className="flex-1">
             <Video />
           </div>
-          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            {modules.map((module, index) => {
-              return (
-                <Module
-                  key={module.id}
-                  moduleIndex={index}
-                  title={module.title}
-                  amountOfLessons={module.lessons.length}
-                />
-              )
-            })}
+          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar  scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+            {modules &&
+              modules.map((module, index) => {
+                return (
+                  <Module
+                    key={module.id}
+                    moduleIndex={index}
+                    title={module.title}
+                    amountOfLessons={module.lessons.length}
+                  />
+                )
+              })}
           </aside>
         </main>
       </div>
